c399bbfc50f8541e7cf078d2fae362e2ca267e0a
Your Name <you@example.com>
2022-09-10 15:19:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config()
+const express = require('express')
+const axios = require("axios")
+const app = express()
+
+const { TELEGRAM_TOKEN, SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
+const URI = `/webhook/${TELEGRAM_TOKEN}`
+const WEBHOOK_URL = SERVER_URL+URI
+
+app.use(express.urlencoded({ 
+    extended: false 
+}))
+
+app.use(express.json())
+
+const init = async () => {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+    console.log(res.data)
+}
+
+app.post(URI, async (req, res) =>{
+    // console.log(req.body)
+
+    const chatId = req.body.message.chat.id
+    const message = req.body.message.text
+
+    // console.log(chatId)
+
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+        chat_id: chatId,
+        text: message
+    })
+    return res.send()
+})
+
+app.listen(process.env.PORT || 5000, async () =>{
+    console.log(`app is running on port`, process.env.PORT  || 5000);
+    await init()
+})
+
+
